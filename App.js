46a35b7f1f216145a5b9a28ded94af25a21ca70d
Mona Zheng <mona.a.zheng@gmail.com>
2020-10-31 00:46:21 -0400

    add: reset btn

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import {
   Text,
   StatusBar,
   TouchableOpacity,
+  Button,
+  View,
 } from 'react-native';
 
 
@@ -27,9 +29,18 @@ const App: () => React$Node = () => {
             onPress={()=> addOne(count, setCount)}
             style={styles.clickableCountButton}
           >
-          <Text style={styles.countText}>{count}</Text>
+            <Text style={styles.countText}>{count}</Text>
           </TouchableOpacity>
-        {/* </View> */}
+          <View style={{ flexDirection: 'row', flex: .05}}>
+            <View style={{flex: 4}}/>
+            <TouchableOpacity 
+              onPress={() => {setCount(0)}}
+              style={styles.resetBtn}
+            >
+              <Text style={styles.resetText}>Reset</Text>
+            </TouchableOpacity>
+            <View style={{flex: .2}}/>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -47,7 +58,6 @@ const styles = StyleSheet.create({
   },
   clickableCountButton: {
     flex: 1,
-    backgroundColor: '#472d30',
     justifyContent: 'center',
     
   },
@@ -56,6 +66,24 @@ const styles = StyleSheet.create({
     fontSize: 100,
     color: 'beige',
     fontWeight: '400',
+  },
+  resetBtn: {
+    flex: 1.3,
+    backgroundColor: '#623d46',
+    borderTopWidth: 2.5,
+    borderBottomWidth: 2.5,
+    borderColor: '#402f39',
+    borderRadius: 7,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  },
+  resetText: {
+    flex: 1,
+    fontSize: 17,
+    fontWeight: '400',
+    color: 'beige',
+    textAlign: 'center'
   },
 });
 
